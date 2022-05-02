45b13e91804e7c0b05122a30fd3f24c2f465608b
Jofell Gallardo <jofellxcite@gmail.com>
2022-05-03 07:40:39 +0800

    Changing attributes to see if it works out

--- lib/class-table-inheritance/inherits-migration.rb
+++ lib/class-table-inheritance/inherits-migration.rb
@@ -15,9 +15,9 @@ module InheritsMigration
         association_inst = association_type.send(:new)
         attr_column = association_inst.column_for_attribute(association_type.primary_key)
         
-        field_option = {:primary_key => true, :null => false}
+        field_option = {type: attr_column.type, :primary_key => true, :null => false}
         field_option[:limit] = attr_column.limit if attr_column.limit                
-        table_defintion.column "#{column_to_create}_id", attr_column.type, field_option
+        table_defintion.column "#{column_to_create}_id", field_option
       end
       yield table_defintion  
     end 
